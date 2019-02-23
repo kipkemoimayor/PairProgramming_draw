a0d959f4ee6320b524b01b88befff818c9b14609
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-02-23 22:32:42 +0300

    Added a gif and h4

--- js/pair.js
+++ js/pair.js
@@ -1,7 +1,7 @@
 function pair(){
-var arrEmpt=["collins","november","etyang","clive","felix","komar","kerry","norman","mcKanzi","ian","kerr","vwan","khat","mai","owa","mali","wawie","kawie"];
+var arrEmpt=["rehemafaith01", "dmontetproff", "mainamurage21", "kingkgdave", "danolago", " newtontush", "aisajemila", "ck.caleb17", "lilowesh.lw", "machoguabednego6", " dicksonparlmaom", " cmmaina", " jonesmwas35", "doktatech2", "alexomaset711", "mugambi117", "reesalumasa", "javanmega0", "willgenuis15", "orukopius8", "mnzinga888", "njuguna13", "johnvictor0002", "kephraim19", "iankajiji", "bjnzivu", "leahgakii74", "jlmukono", "husseinfuaad", "marshalldennis27", "wambitafana", "judyrop97", "dorismtae", "kibetkirui010", "asmaahmed.adan", "anthony.ahere", "hopemutheu", "adhoadhigal", "badrubadri92", "eugenesakwasaudababs00", "eric.kingoo2014", "agnesndutamwaniki", "misedaalex", "teenahshyro", "amiredi", "collinskipkemoi24", "emmaKibore", "amiinegal3"];
 var output="";
-for(i=0;i<9;i++){
+for(i=0;i<24;i++){
 var randomW=Math.floor(Math.random()*arrEmpt.length);
 arrEmpt[randomW];
 var pairOne=arrEmpt.splice(randomW,1)
@@ -9,9 +9,9 @@ var random=Math.floor(Math.random()*arrEmpt.length);
 arrEmpt[random];
 var pairTwo=arrEmpt.splice(random,1);
 
-document.getElementById('pa').innerHTML=pairOne+" vs "+pairTwo;
+document.getElementById('pa').innerHTML=pairOne+"  vs  "+pairTwo;
 //document.write("<br>"+pairOne+" vs "+pairTwo);
-output=output+"<br>"+pairOne+" vs "+pairTwo;
+output=output+"<br> \n "+pairOne+" vs "+pairTwo;
 }
 document.getElementById('pa').innerHTML=output;
 var keya=[];
@@ -19,3 +19,9 @@ var newKey=keya.push(output);
 
 }
 pair();
+$(document).ready(function(){
+  $("button").click(function(){
+      $(".show").slideDown(2000);
+      $(".show").show();
+  })
+});
